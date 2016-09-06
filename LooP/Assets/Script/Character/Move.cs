3e022aca42231adeb15a4d16b8959e49a0779089
hirooka <g1345143@cc.kyoto-su.ac.jp>
2016-09-06 22:45:38 +0900

    左右に移動
    カメラも付いてくる！

---
+++ LooP/Assets/Script/Character/Move.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// キャラの移動系スクリプト
+/// </summary>
+public class Move : MonoBehaviour {
+	/// <summary>
+	/// スピードの構造体
+	/// </summary>
+	private struct Speed {
+		public float x, y;
+		public Speed(float x, float y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	private Speed speed = new Speed(0.1f, 0.1f);
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update () {
+		// 左右移動
+		Vector2 Position = transform.position;
+		if (Input.GetKey ("left")) {
+			Position.x -= speed.x;
+		} 
+		else if (Input.GetKey ("right")) {
+			Position.x += speed.x;
+		}
+		transform.position = Position;
+	}
+}
