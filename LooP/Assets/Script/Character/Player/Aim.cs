5e8674a1a940836a5e8ea93615c900c93e4687f3
hirooka <g1345143@cc.kyoto-su.ac.jp>
2016-09-08 04:48:10 +0900

    エイムの途中

---
+++ LooP/Assets/Script/Character/Player/Aim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("AimH") == 0 && Input.GetAxis ("AimV") == 0)
+			return;
+		Vector2 v = transform.position;
+		v.x = transform.parent.position.x + Input.GetAxis ("AimH");
+		v.y = transform.parent.position.y + Input.GetAxis ("AimV");
+		transform.position = v;
+	
+	}
+}
