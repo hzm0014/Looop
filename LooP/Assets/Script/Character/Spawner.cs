4cd67d3e3571b5149e5751e6250b79ff61444fc7
hirooka <g1345143@cc.kyoto-su.ac.jp>
2016-11-03 16:30:31 +0900

    Merge branch 'master' into 部屋づくり
    # Conflicts:
    #	LooP/Assets/Game.unity
    #	LooP/ProjectSettings/ProjectSettings.asset
    #	LooP/ProjectSettings/ProjectVersion.txt

--- LooP/Assets/Script/Character/Spawner.cs
+++ LooP/Assets/Script/Character/Spawner.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using System.Collections;
 
 public class Spawner : MonoBehaviour {
-	
 	//発生するObjectを指定する用
 	public GameObject spawnObject;
 	//発生感覚
@@ -25,7 +24,29 @@ public class Spawner : MonoBehaviour {
 		}
 	}
 	
+	// [S1] popの間隔の設定
 	void SetInterval(float interval) {
 		this.interval = interval;
 	}
+	// [S2] SpawnObjectの設定
+	void SetSpawnObject(GameObject go) {
+		spawnObject = go;
+	}
+	// [S3] popするかの設定
+	void SetIsAvailable(bool IA) {
+		isSpawn = IA;
+	}
+	// [Initialize] 初期化関数 or まとめて変更関数
+	void init(float interval, GameObject go, bool IA) {
+		SetInterval (interval);
+		SetSpawnObject(go);
+		SetIsAvailable(IA);
+	}
+	
+	// [G1] pop間隔を返す
+	float GetInterval() { return interval; }
+	// [G2] spawnする物を返す
+	GameObject GetSpawnObject() { return spawnObject; }
+	// [G3] popするかを返す
+	bool IsAvailable() { return isSpawn; }
 }
