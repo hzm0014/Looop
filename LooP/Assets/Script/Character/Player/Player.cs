35e3bd88d91620362febd45f25e8325c061a7fcf
Jazz <g1344658@cse.kyoto-su.ac.jp>
2016-11-04 02:41:13 +0900

    発射ノックバック

--- LooP/Assets/Script/Character/Player/Player.cs
+++ LooP/Assets/Script/Character/Player/Player.cs
@@ -48,12 +48,15 @@ public class Player : Character {
 	
 	// 特技；射撃
 	public override void Specialty () {
-		GameObject obj = (GameObject)Instantiate (Resources.Load ("Prefavs/Bullet/Kunai"));
-		obj.GetComponent <Kunai> ().SetBullet(transform.position, aim.transform.localEulerAngles);
+		GameObject obj = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+		Kunai kunai = obj.GetComponent<Kunai>();
+		Debug.Log(kunai.transform.rotation);
+		kunai.SetBullet(transform.position, aim.transform.localEulerAngles);
 		// 吹っ飛び
 		Vector3 aimVec = aim.transform.localEulerAngles;
-		Debug.Log(aimVec);
-		GetComponent<Rigidbody2D>().AddForce(new Vector2(aimVec.x, aimVec.y), ForceMode2D.Impulse);
+		Vector2 kunaiVec = new Vector2( -1*Mathf.Cos (Mathf.Deg2Rad * aimVec.z) *5.0f ,  -1*Mathf.Sin (Mathf.Deg2Rad * aimVec.z)*10.0f );
+		//Debug.Log(kunaiVec);
+		GetComponent<Rigidbody2D>().AddForce(kunaiVec, ForceMode2D.Impulse);
 	}
 	//ダメージ
 	public override void Damage(IDamageGenerator damageGenerator){
