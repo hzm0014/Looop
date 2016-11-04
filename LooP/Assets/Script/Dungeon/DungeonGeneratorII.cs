0d02cd7ffa1c6c9a7240690bc4b8e698c395e455
Jazz <g1344658@cse.kyoto-su.ac.jp>
2016-11-04 09:51:27 +0900

    ボスの体力の最大値と最小値に変化を

--- LooP/Assets/Script/Dungeon/DungeonGeneratorII.cs
+++ LooP/Assets/Script/Dungeon/DungeonGeneratorII.cs
@@ -136,7 +136,7 @@ public class DungeonGeneratorII : Singleton<DungeonGeneratorII> {
 		int goal = 5;
 		SetBigWall (start, goal);
 		// ボスを戦闘モードに
-		GameObject.Find ("Boss(Clone)").GetComponent<Boss>().ShiftButtleMode(100*(_floorNum/10), 50, 100*(_floorNum/10));
+		GameObject.Find ("Boss(Clone)").GetComponent<Boss>().ShiftButtleMode(100*(_floorNum/10), 50, 90*(_floorNum/10));
 		// ステージ生成
 		stage = (GameObject)Instantiate (BossStage, new Vector2((float)width/2, (float)height/2), new Quaternion ());
 		stage.transform.parent = gameObject.transform;
