444f10b9fc51e5fe265680a6a10cc6bdbcd4cebd
hirooka <g1345143@cc.kyoto-su.ac.jp>
2016-11-05 11:13:34 +0900

    ハイスコアを実装
    ハイスコアならファイルを書き出して保存
    mac以外の環境での動作確認はしていない

--- LooP/Assets/Script/MaxRecord.cs
+++ LooP/Assets/Script/MaxRecord.cs
@@ -1,35 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Text;
 using System.IO;
 
 public class MaxRecord : Singleton<MaxRecord> {
-	string name = "a";
 	protected MaxRecord() {}
-	public GameObject ui;
 	private int record;
 
 	public bool DontDestroyEnabled = true;
 
 	// Use this for initialization
 	void Start () {
-
-		var folderpath = Application.persistentDataPath + "/Database/";
-		var filePath = folderpath + name + ".json";
-		if (!File.Exists (filePath)) return;
-
-		var jsonText = File.ReadAllText (filePath);
-		if (string.IsNullOrEmpty (jsonText)) return;
-
-		/// Json文字列をCharacterDataの配列に変換する
-		var json = JsonUtility.FromJson<int> (jsonText);
-		record = (int)json;
-
+		StreamReader sr = new StreamReader (
+		@"C:\record.txt", Encoding.GetEncoding ("Shift_JIS"));
+		SetRecord (int.Parse(sr.ReadLine ()));
+		sr.Close ();
 	}
 
 	public void SetRecord(int entry) {
 		record = Mathf.Max (record, entry);
-		ui.GetComponent<Text> ().text = "最高記録 B " + record;
+		GetComponent<Text> ().text = "HIGHT SCORE :B" + record;
+		FileWrite ();
 
 	}
 
@@ -37,17 +29,12 @@ public class MaxRecord : Singleton<MaxRecord> {
 	/// ファイルに書き出す
 	/// </summary>
 	public void FileWrite () {
-		string json = JsonUtility.ToJson (record);
-
-		// 保存するフォルダー
-		var path = Application.persistentDataPath + "/Database/";
-
-		// フォルダーがない場合は作成する
-		if (!Directory.Exists (path)) {
-			Directory.CreateDirectory (path);
-			}
-
-		File.WriteAllText (path + name + ".json", json);
+		System.IO.File.Delete (@"C:\record.txt");
+		Encoding sjisEnc = Encoding.GetEncoding ("Shift_JIS");
+		StreamWriter writer =
+		   new StreamWriter (@"C:\record.txt", true, sjisEnc);
+		writer.Write (record);
+		writer.Close ();
 	}
 
 }
